8c18cfefcd07b17e1d61c154122f50d62ed39ade
Владислав <tolstovvvlad335@gmai.com>
2024-10-15 17:24:07 +0900

    довил обработку на отсутсвие api

---
+++ src/pages/Tools/NotConnected/NotConnected.vue
@@ -0,0 +1,7 @@
+<template>
+  <h2 class="text-[32px]">Not connected</h2>
+</template>
+<script>
+export default {};
+</script>
+<style lang=""></style>
--- src/pages/Tools/ToolsPage.vue
+++ src/pages/Tools/ToolsPage.vue
@@ -1,5 +1,9 @@
 <template>
-  <div class="tools my-container overflow-scroll h-[75vh] relative">
+  <div v-if="apiLength === 0" class="h-[70%] flex justify-center items-center">
+    <NotConnected />
+  </div>
+
+  <div v-else class="tools my-container overflow-scroll h-[75vh] relative">
     <div class="mb-[15px]">
       <MainTItle :text="'mailing tools'" />
     </div>
@@ -10,6 +14,10 @@
 import MainTItle from "@/components/Titles/MainTItle.vue";
 import ToolsForm from "../../components/Forms/ToolsForm/ToolsForm.vue";
 import { useStore } from "vuex";
+import NotConnected from "./NotConnected/NotConnected.vue";
+import { computed } from "vue";
 
 const store = useStore();
+
+let apiLength = computed(() => store.getters["getApi"].length);
 </script>
